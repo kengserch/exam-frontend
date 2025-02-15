93ad76f2fd3857e0c9100408651600a2750de9b2
Prasert Saengkaew <kengsert@gmail.com>
2025-02-15 11:09:28 +0700

    toggle darkmode

--- exam4-darkmode-toggle/src/App.jsx
+++ exam4-darkmode-toggle/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const storedTheme = localStorage.getItem('theme') || 'light';
+    const [theme, setTheme] = useState(storedTheme);
+    const [isChecked, setIsChecked] = useState(storedTheme == 'dark');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+        setIsChecked(theme === 'dark');
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+    };
+
+    const DarkMode = () => {
+        return (
+            <div className="flex items-center justify-center p-4">
+                <span className="text-4xl">☀️</span>
+                <label className="relative inline-block h-8 w-20">
+                    <input
+                        type="checkbox"
+                        className="hidden"
+                        checked={isChecked}
+                        onChange={toggleTheme}
+                    />
+                    <div
+                        className={`absolute inset-0 cursor-pointer rounded-full 
+                transition duration-500 ease-in-out
+                ${isChecked ? 'bg-blue-500' : 'bg-gray-300'}`}
+                    >
+                        <div
+                            className={`absolute left-1 top-1 h-6 w-6 rounded-full bg-white
+                  transition-transform duration-500 ease-in-out
+                  ${isChecked ? 'translate-x-12' : 'translate-x-0'}`}
+                        />
+                    </div>
+                </label>
+                <span className="text-4xl">🌒</span>
+            </div>
+        );
+    };
+
+    return (
+        <>
+            <DarkMode />
+        </>
+    );
 }
 
-export default App
+export default App;
